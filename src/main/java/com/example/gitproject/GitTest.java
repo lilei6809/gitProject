b696a21966fa34aadb2c42c086343e891524ef24
Leland <lilei6809@gmail.com>
2021-09-18 18:22:26 +0800

    git test second commit

--- src/main/java/com/example/gitproject/GitTest.java
+++ src/main/java/com/example/gitproject/GitTest.java
@@ -4,12 +4,11 @@ public class GitTest {
 
     public static void main(String [] args){
 
+        System.out.println("hot-fix test!");
+
         System.out.println("hot-fix");
-
         System.out.println("hello git!");
-
         System.out.println("version 1.1");
-
         System.out.println("version 1.2");
         System.out.println("version 1.3");
     }
