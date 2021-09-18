b90bda74bfb4a771afd1b56f156f23e285cef016
Leland <lilei6809@gmail.com>
2021-09-18 18:24:16 +0800

    git master test commit

--- src/main/java/com/example/gitproject/GitTest.java
+++ src/main/java/com/example/gitproject/GitTest.java
@@ -4,6 +4,9 @@ public class GitTest {
 
     public static void main(String [] args){
 
+
+        System.out.println("master test!");
+
         System.out.println("hot-fix");
 
         System.out.println("hello git!");
