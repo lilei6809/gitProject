e72c339061b771bf024b3ad23f919286703d4f0e
Leland <lilei6809@gmail.com>
2021-09-18 18:37:45 +0800

    git test2 commit

--- src/main/java/com/example/gitproject/GitTest.java
+++ src/main/java/com/example/gitproject/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
     public static void main(String [] args){
 
         System.out.println("push test");
-
+        System.out.println("push test2");
         System.out.println("master test!");
 
         System.out.println("hot-fix test!");
